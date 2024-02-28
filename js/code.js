21fdc9b676e845e49da850bcc178a9ed2dd74c56
feriets <160668170+feriets@users.noreply.github.com>
2024-02-28 11:13:57 -0500

    Corrigendo errores en el código JS

--- js/code.js
+++ js/code.js
@@ -6,74 +6,98 @@ const jueves = document.querySelectorAll('.jueves')
 const viernes = document.querySelectorAll('.viernes')
 const sabado = document.querySelectorAll('.sabado')
 
-// --------------------------------------------------------------
+const _1lunes = document.querySelector('.current_activity1lunes')
+const _1martes = document.querySelector('.current_activity1martes')
+const _1miercoles = document.querySelector('.current_activity1miercoles')
+const _1jueves = document.querySelector('.current_activity1jueves')
+const _1viernes = document.querySelector('.current_activity1viernes')
+const _1sabado = document.querySelector('.current_activity1sabado')
 
-const current_activity1lunes = document.querySelector('current_activity1lunes')
-const current_activity1martes = document.querySelector('current_activity1martes')
-const current_activity1miercoles = document.querySelector('current_activity1miercoles')
-const current_activity1jueves = document.querySelector('current_activity1jueves')
-const current_activity1viernes = document.querySelector('current_activity1viernes')
-const current_activity1sabado = document.querySelector('current_activity1sabado')
+const _2lunes = document.querySelector('.current_activity2lunes')
+const _2martes = document.querySelector('.current_activity2martes')
+const _2miercoles = document.querySelector('.current_activity2miercoles')
+const _2jueves = document.querySelector('.current_activity2jueves')
+const _2viernes = document.querySelector('.current_activity2viernes')
+const _2sabado = document.querySelector('.current_activity2sabado')
 
-const current_activity2lunes = document.querySelector('current_activity2lunes')
-const current_activity2martes = document.querySelector('current_activity2martes')
-const current_activity2miercoles = document.querySelector('current_activity2miercoles')
-const current_activity2jueves = document.querySelector('current_activity2jueves')
-const current_activity2viernes = document.querySelector('current_activity2viernes')
-const current_activity2sabado = document.querySelector('current_activity2sabado')
+const _3lunes = document.querySelector('.current_activity3lunes')
+const _3martes = document.querySelector('.current_activity3martes')
+const _3miercoles = document.querySelector('.current_activity3miercoles')
+const _3jueves = document.querySelector('.current_activity3jueves')
+const _3viernes = document.querySelector('.current_activity3viernes')
+const _3sabado = document.querySelector('.current_activity3sabado')
 
-const current_activity3lunes = document.querySelector('current_activity3lunes')
-const current_activity3martes = document.querySelector('current_activity3martes')
-const current_activity3miercoles = document.querySelector('current_activity3miercoles')
-const current_activity3jueves = document.querySelector('current_activity3jueves')
-const current_activity3viernes = document.querySelector('current_activity3viernes')
-const current_activity3sabado = document.querySelector('current_activity3sabado')
+const _4lunes = document.querySelector('.current_activity4lunes')
+const _4martes = document.querySelector('.current_activity4martes')
+const _4miercoles = document.querySelector('.current_activity4miercoles')
+const _4jueves = document.querySelector('.current_activity4jueves')
+const _4viernes = document.querySelector('.current_activity4viernes')
+const _4sabado = document.querySelector('.current_activity4sabado')
 
-const current_activity4lunes = document.querySelector('current_activity4lunes')
-const current_activity4martes = document.querySelector('current_activity4martes')
-const current_activity4miercoles = document.querySelector('current_activity4miercoles')
-const current_activity4jueves = document.querySelector('current_activity4jueves')
-const current_activity4viernes = document.querySelector('current_activity4viernes')
-const current_activity4sabado = document.querySelector('current_activity4sabado')
+const _5lunes = document.querySelector('.current_activity5lunes')
+const _5martes = document.querySelector('.current_activity5martes')
+const _5miercoles = document.querySelector('.current_activity5miercoles')
+const _5jueves = document.querySelector('.current_activity5jueves')
+const _5viernes = document.querySelector('.current_activity5viernes')
+const _5sabado = document.querySelector('.current_activity5sabado')
 
-const current_activity5lunes = document.querySelector('current_activity5lunes')
-const current_activity5martes = document.querySelector('current_activity5martes')
-const current_activity5miercoles = document.querySelector('current_activity5miercoles')
-const current_activity5jueves = document.querySelector('current_activity5jueves')
-const current_activity5viernes = document.querySelector('current_activity5viernes')
-const current_activity5sabado = document.querySelector('current_activity5sabado')
+const _6lunes = document.querySelector('.current_activity6lunes')
+const _6martes = document.querySelector('.current_activity6martes')
+const _6miercoles = document.querySelector('.current_activity6miercoles')
+const _6jueves = document.querySelector('.current_activity6jueves')
+const _6viernes = document.querySelector('.current_activity6viernes')
+const _6sabado = document.querySelector('.current_activity6sabado')
 
-const current_activity6lunes = document.querySelector('current_activity6lunes')
-const current_activity6martes = document.querySelector('current_activity6martes')
-const current_activity6miercoles = document.querySelector('current_activity6miercoles')
-const current_activity6jueves = document.querySelector('current_activity6jueves')
-const current_activity6viernes = document.querySelector('current_activity6viernes')
-const current_activity6sabado = document.querySelector('current_activity6sabado')
-
-const current_activity7lunes = document.querySelector('current_activity7lunes')
-const current_activity7martes = document.querySelector('current_activity7martes')
-const current_activity7miercoles = document.querySelector('current_activity7miercoles')
-const current_activity7jueves = document.querySelector('current_activity7jueves')
-const current_activity7viernes = document.querySelector('current_activity7viernes')
-const current_activity7sabado = document.querySelector('current_activity7sabado')
-
-
-// --------------------------------------------------------------
+const _7lunes = document.querySelector('.current_activity7lunes')
+const _7martes = document.querySelector('.current_activity7martes')
+const _7miercoles = document.querySelector('.current_activity7miercoles')
+const _7jueves = document.querySelector('.current_activity7jueves')
+const _7viernes = document.querySelector('.current_activity7viernes')
+const _7sabado = document.querySelector('.current_activity7sabado')
 
 const date_current = document.getElementById('date_current')
 
 
-function MarkActivityCurrent() {
+function MarkTable(one, two, three, four, five, six, seven) {
     const hour = new Date();
     const currentHour = hour.getHours();
     const currentMinute = hour.getMinutes();
 
-    if (currentHour === 16 && currentMinute >= 0 && currentMinute <= 50) {
-        current_activity5martes.style.backgroundColor = 'red';
-        console.log(current_activity5martes)
+    if (currentHour === 6 && currentMinute >= 0 && currentMinute <= 59) {
+        one.style = 'background-color: #35b34a'
+    } else if (currentHour === 7 && currentMinute >= 0 && currentMinute <= 59){
+        two.style = 'background-color: #35b34a'
+    } else if (currentHour === 8 && currentMinute >= 30 && currentMinute <= 59){
+        three.style = 'background-color: #35b34a'
+    } else if (currentHour === 17 && currentMinute >= 0 && currentMinute <= 30){
+        four.style = 'background-color: #35b34a'
+    } else if (currentHour === 19 && currentMinute >= 0 && currentMinute <= 59){
+        five.style = 'background-color: #35b34a'
+    } else if (currentHour === 20 && currentMinute >= 0 && currentMinute <= 59){
+        six.style = 'background-color: #35b34a'
+    } else if (currentHour === 21 && currentMinute >= 0 && currentMinute <= 59){
+        seven.style = 'background-color: #35b34a'
     }
 }
 
+function CallFunctions(){
+    let days = ['', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    const day_current = new Date()
+
+    if (days[day_current.getDay()] === 'Lunes'){
+        MarkTable(_1lunes, _2lunes, _3lunes, _4lunes, _5lunes, _6lunes, _7lunes)
+    } else if (days[day_current.getDay()] === 'Martes'){
+        MarkTable(_1martes, _2martes, _3martes, _4martes, _5martes, _6martes, _7martes)
+    } else if (days[day_current.getDay()] === 'Miercoles'){
+        MarkTable(_1miercoles, _2miercoles, _3miercoles, _4miercoles, _5miercoles, _6miercoles, _7miercoles)
+    } else if (days[day_current.getDay()] === 'Jueves'){
+        MarkTable(_1jueves, _2jueves, _3jueves, _4jueves, _5jueves, _6jueves, _7jueves)
+    } else if (days[day_current.getDay()] === 'Viernes'){
+        MarkTable(_1viernes, _2viernes, _3viernes, _4viernes, _5viernes, _6viernes, _7viernes)
+    } else if (days[day_current.getDay()] === 'Sabado'){
+        MarkTable(_1sabado, _2sabado, _3sabado, _4sabado, _5sabado, _6sabado, _7sabado)
+    }
+}
 
 function ShowDayCurrent() {
     let days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
@@ -128,8 +152,7 @@ function ShowDayCurrent() {
 }
 
 
-
 ShowDayCurrent()
-MarkActivityCurrent()
+CallFunctions()
 
 
