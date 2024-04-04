655d91cef69bcb4c97a83683c04999a00e962017
feriets <160668170+feriets@users.noreply.github.com>
2024-04-04 16:15:40 -0500

    Actualizando horario y algunos cambios especificos

--- js/code.js
+++ js/code.js
@@ -183,6 +183,15 @@ function rule_days(){
     }
 }
 
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('.nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
 
 ShowDayCurrent()
 CallFunctions()
